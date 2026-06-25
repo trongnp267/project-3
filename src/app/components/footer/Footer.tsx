f45550d90e1ebbda1ab5521534ededc28d95a1aa
Trong Nguyen <trongnp@gmail.com>
2026-06-25 20:39:37 +0700

    Dựng Footer

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-[#000065] py-[24px]">
+      <div className="contain">
+        <div className="font-[800] text-[28px] text-white text-center mb-[10px]">
+          IT.Jobs
+        </div>
+        <div className="font-[400] text-[14px] text-[#A6A6A6] text-center">
+          Copyright © IT VIEC JSC
+        </div>
+      </div>
+    </footer>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Header } from "./components/header/Header";
+import { Footer } from "./components/footer/Footer";
 
 export const metadata: Metadata = {
   title: "IT Jobs",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
