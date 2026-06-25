ff9b1a8a363c11ef207cd3f0b3558b84924aadb9
Trong Nguyen <trongnp@gmail.com>
2026-06-25 12:58:07 +0700

    Responsive giao diện Header

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
-import { FaAngleDown, FaAngleRight } from "react-icons/fa"
+import { FaAngleDown, FaAngleRight, FaBars } from "react-icons/fa"
 
 export const Header = () => {
   return (
     <>
       <header className="bg-[#000071] py-[15px]">
         <div className="contain">
-          <div className="flex items-center justify-between">
-            <Link href="/" className="text-[#FFFFFF] font-[800] text-[28px]">IT.Jobs</Link>
+          <div className="flex items-center lg:justify-between">
+            <Link href="/" className="text-[#FFFFFF] font-[800] sm:text-[28px] text-[20px] lg:flex-none flex-1">IT.Jobs</Link>
             <nav className="menu">
               <ul>
                 <li>
@@ -74,11 +74,14 @@ export const Header = () => {
                 </li>
               </ul>
             </nav>
-            <div className="flex gap-[5px] text-[#FFFFFF] font-[600] text-[16px]">
+            <div className="flex gap-[5px] text-[#FFFFFF] font-[600] sm:text-[16px] text-[12px]">
               <Link href="#">Đăng Nhập</Link>
               <span>/</span>
               <Link href="#">Đăng Ký</Link>
             </div>
+            <button className="text-white text-[23px] lg:hidden ml-[12px] cursor-pointer">
+              <FaBars />
+            </button>
           </div>
         </div>
       </header>
