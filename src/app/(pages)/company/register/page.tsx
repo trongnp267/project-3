2fbe6d961d959e5af88fccfe16134b4c2de22f7c
Trong Nguyen <trongnp@gmail.com>
2026-06-28 12:31:57 +0700

    Dựng trang Đăng ký (Nhà tuyển dụng)

---
+++ src/app/(pages)/company/register/page.tsx
@@ -0,0 +1,43 @@
+import { Button1 } from "@/app/components/button/Button1";
+import { InputGroup } from "@/app/components/form/InputGroup";
+
+export default function CompanyRegister() {
+  return (
+    <>
+      <div className="py-[60px] sm:min-h-[calc(100vh-121px-72px)] min-h-[calc(100vh-121px-60px)]">
+        <div className="contain">
+          <div className="bg-white border border-[#DEDEDE] py-[50px] px-[20px] rounded-[8px] w-full max-w-[602px] mx-[auto]">
+            <h1 className="font-[700] text-black text-[20px] mb-[20px] text-center">
+              Đăng ký (Nhà tuyển dụng)
+            </h1>
+            <form 
+              action=""
+              className="grid gap-[15px]"
+            >
+              <InputGroup
+                name=""
+                id=""
+                label="Tên công ty *"
+              />
+              <InputGroup
+                name=""
+                id=""
+                label="Email *"
+                type="email"
+              />
+              <InputGroup
+                name=""
+                id=""
+                label="Mật khẩu *"
+                type="password"
+              />
+              <Button1
+                label="Đăng ký"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
